004505fd303840b1e952f724053623a2923b43ad
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-06-15 14:36:08 -0700

    Basic JavaScript Project
    Added index.html page and main.js

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var A = "This is a string. "; // Defining variable, giving string value
+var B = "Concatenated" + " string. "; // Defining variable, giving concatenated string value
+var Work = "Logistics Company", Manager = "Kyle", Specialist = "Alyssa", Assistant1  = "Zach", Assistant2 = "Drew"; // Defining multiple variables in single statement
+
+window.alert('Howdy, world.'); // Enabling a window alert
+
+document.write(A); // Displaying the value of variable A
+document.write(B); // Displaying the value of variable B
+
+document.write("<br> <br> <br>"); // Multiple line breaks
+
+document.write("I work at " + Work + " and " + Assistant1 + " and " + Assistant2 + " are on my team."); // Displaying concatenated variables with string values
+
+document.write("<br> <br> <br>"); // Multiple line breaks
+
+document.write("\"We choose to go to the moon,\" said John F. Kennedy,"); // Displaying first part of a quote
+document.write(" \"We choose to go to the moon in this decade and do the other things, not because they are easy," // Displaying second part of
++ " but because they are hard, because that goal will serve to organize and measure the best of our energies and skills," // quote by
++ " because that challenge is one that we are willing to accept, one we are unwilling to postpone," // concatenating multiple
++ "and one which we intend to win, and the others, too.\""); // string values together
+
+document.write("<br> <br> <br>") // Multiple line breaks to separate quote and variable
+
+document.write(5+5); // Displaying value of expression (5+5)
