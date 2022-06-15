37cfde6a4e910c92977064bdcd50376ce88ec5e7
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-06-15 16:48:51 -0700

    Basic JavaScript Submission
    Basic JavaScript Project

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -7,11 +7,15 @@ window.alert('Howdy, world.'); // Enabling a window alert
 document.write(A); // Displaying the value of variable A
 document.write(B); // Displaying the value of variable B
 
-document.write("<br> <br> <br>"); // Multiple line breaks
+    document.write("<br> <br> <br>"); // Multiple line breaks
+
+document.write(A + B); // Displaying the values of variables A and B
+
+    document.write("<br> <br> <br>"); // Multiple line breaks
 
 document.write("I work at " + Work + " and " + Assistant1 + " and " + Assistant2 + " are on my team."); // Displaying concatenated variables with string values
 
-document.write("<br> <br> <br>"); // Multiple line breaks
+    document.write("<br> <br> <br>"); // Multiple line breaks
 
 document.write("\"We choose to go to the moon,\" said John F. Kennedy,"); // Displaying first part of a quote
 document.write(" \"We choose to go to the moon in this decade and do the other things, not because they are easy," // Displaying second part of
@@ -19,6 +23,6 @@ document.write(" \"We choose to go to the moon in this decade and do the other t
 + " because that challenge is one that we are willing to accept, one we are unwilling to postpone," // concatenating multiple
 + "and one which we intend to win, and the others, too.\""); // string values together
 
-document.write("<br> <br> <br>") // Multiple line breaks to separate quote and variable
+    document.write("<br> <br> <br>") // Multiple line breaks to separate quote and variable
 
 document.write(5+5); // Displaying value of expression (5+5)
