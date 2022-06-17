588d6b31123964c8be455bef47cd18f9a068ff70
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-06-17 16:47:28 -0700

    Basic JavaScript Projects
    Project 5 Comparisons Type Coercion Submission Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main5.js
@@ -0,0 +1,27 @@
+var A = 10; // Defining variables,
+var B = 15; // and their values
+document.write(A === B); // Displaying comparison
+
+            document.write("<br>");
+
+document.write("Fifty" + 50); // Displaying combination of a string and number
+
+            document.write("<br>");
+
+document.write(5 == 5); // Displaying comparison
+
+            document.write("<br>");
+
+document.write(10 > 4 && 3 < 20); // Displaying comparison
+
+            document.write("<br>");
+
+document.write(30 > 10 || 20 < 44); // Displaying comparison
+
+            document.write("<br>");
+
+document.write(12 > 3 != 3 < 12); // Displaying comparison
+
+            document.write("<br>");
+
+document.write(typeof 23); // Displaying comparison
