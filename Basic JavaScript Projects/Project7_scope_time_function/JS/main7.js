b3a5e8da8cd5d1fd823281942b6e53bd580de36f
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-06-19 05:23:05 -0700

    Basic JavaScript Projects
    Project 7 Scope Time Function Submission Assignment

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main7.js
@@ -0,0 +1,61 @@
+// GLOBAL VARIABLE
+var X = 10;
+function Add_numbers_1()    {
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2()    {
+    document.write(X + 20);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+    document.write("<br><br><br>");
+
+
+// LOCAL VARIABLE
+function Add_numbers_1()    {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2()    {
+    document.write(X + 20);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+    document.write("<br><br><br>");
+
+
+// IF STATEMENT
+function myFunction()   {
+    if (5>3)    {
+    document.write("The number on the left is greater than the number on the right.");
+    }
+}
+
+
+// TIME FUNCTION
+function Time_function()    {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth()+1) + '-'+ today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    document.write("Today is " + dateTime);
+
+
+    document.write("<br><br><br>");
+}
+
+// FUNCTION W/O ERROR
+var C = "This is a string value";
+document.write(C);
+console.log(C)
+
+
+document.write("<br><br><br>");
+
+
+    // FUNCTION WITH ERROR
+    var C = "This is a string value";
+    C();
+    console.log(C) // Uncaught TypeError: C is not a function at main7.js:45:1
