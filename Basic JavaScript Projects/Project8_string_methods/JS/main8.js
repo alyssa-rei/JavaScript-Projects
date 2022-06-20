d4626a30ff2c843c636ad0bd6e10eb4363ef2769
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-06-20 12:47:28 -0700

    Basic JavaScript Projects
    Updated main8.js

--- Basic JavaScript Projects/Project8_string_methods/JS/main8.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main8.js
@@ -1,30 +1,30 @@
 // Concat() Method
 
-function full_Sentence()    {
-    var part_1 = "I have made ";
-    var part_2 = "this sentence ";
-    var part_3 = "using the ";
-    var part_4 = "concat() method.";
+function full_Sentence()    {       // Naming function
+    var part_1 = "I have made ";    //
+    var part_2 = "this sentence ";  // 
+    var part_3 = "using the ";      // Naming variables, string values
+    var part_4 = "concat() method.";//
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concat").innerHTML = whole_sentence;
+    document.getElementById("Concat").innerHTML = whole_sentence; // Displaying concatenated values
 }
 
 // Slice() Method
 
-function slice_Method() {
-    var Sentence = "This sentence is using the slice() method.";
-    var Section = Sentence.slice(26,34);
-    document.getElementById("Slice").innerHTML = Section;
+function slice_Method() {           // Naming function
+    var Sentence = "This sentence is using the slice() method.";    // Naming variable, string value / sentence
+    var Section = Sentence.slice(26,34);    // Naming variable, start/end values
+    document.getElementById("Slice").innerHTML = Section;   // Displaying sliced value
 }
 
-// toString() Method
-function string_Method()    {
-    var num = 623;
-    document.getElementById("Numbers_to_string").innerHTML = num.toString();
+// toString() Method                
+function string_Method()    {       // Naming function
+    var num = 623;                  // Naming variable, value
+    document.getElementById("Numbers_to_string").innerHTML = num.toString(); // Displaying string, decimal
 }
 
 // toPrecision() Method
-function precision_Method() {
-    var X = 343.3123543;
-    document.getElementById("Precision").innerHTML = X.toPrecision(5);
+function precision_Method() {       // Naming function
+    var X = 343.3123543;            // Naming variable, value
+    document.getElementById("Precision").innerHTML = X.toPrecision(5); // Displaying value to five digits
 }
